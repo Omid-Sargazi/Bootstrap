349f8b0b734bfcf2348927fb01b769e1d2a71450
Omid-Sargazi <om.sargazi@gmail.com>
2024-09-21 19:50:25 +0330

    done javaScript file for modal

--- Motorolla/Modal2/script.js
+++ Motorolla/Modal2/script.js
@@ -1,1 +1,25 @@
 
+var modal = document.getElementById("myModal")
+var btn = document.getElementById("myBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function()
+{
+    modal.style.display="block";
+}
+
+span.onclick = function()
+{
+    console.log("span");
+    modal.style.display="none";
+}
+
+window.onclick = function(event)
+{
+    console.log("window")
+    if(event.target==modal)
+    {
+        modal.style.display="none";
+    }
+}
